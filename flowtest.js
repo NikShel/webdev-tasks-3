fc3a78248e196e89949c4e874a195d99c9cf315f
NikShel <nikshelekb@yandex.ru>
2016-03-16 18:28:04 +0500

    Больше тестов serial, sinon-chai, исправление flow.js

--- flowtest.js
+++ flowtest.js
@@ -3,21 +3,43 @@
 const flow = require('./lib/flow.js');
 const assert = require('assert');
 const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+chai.should();
+chai.use(sinonChai);
+
 
 describe('Flow tests', function () {
     describe('Serial tests', function () {
+        it('should work with empty empty functions list', function () {
+            var callback = sinon.spy();
+            flow.serial([], callback);
+            callback.should.have.been.called;
+        });
+
         it('should run functions in series', function () {
             var callback = sinon.spy();
+            var firstFunc = sinon.spy(function (next) {
+                    next(null, [1]);
+                });
+            var secondFunc = sinon.spy(function (arr, next) {
+                    arr.push(2);
+                    next(null, arr);
+                });
+            var thirdFunc = sinon.spy(function (arr, next) {
+                    arr.push(3);
+                    next(null, arr);
+                });
             var functions = [
-                function (next) {
-                    next(null, 1);
-                },
-                function (data, next) {
-                    next(null, data);
-                }
+                firstFunc,
+                secondFunc,
+                thirdFunc
             ];
             flow.serial(functions, callback);
-            assert.ok(callback.withArgs(null, 1).calledOnce);
+            firstFunc.should.have.been.calledOnce;
+            secondFunc.should.have.been.calledOnce;
+            thirdFunc.should.have.been.calledOnce;
+            callback.should.have.been.calledWithExactly(null, [1, 2, 3]);
         });
 
         var callback = sinon.spy();
@@ -32,17 +54,22 @@ describe('Flow tests', function () {
             shouldNotWork
         ];
         flow.serial(functions, callback);
-
         it('should call callback with error if needed', function () {
-            assert.ok(callback.calledWith('error', null));
+            callback.should.have.been.calledWith('error', null);
         });
 
         it('should stop after error', function () {
-            assert.ok(shouldNotWork.notCalled);
+            shouldNotWork.should.have.not.been.called;
         });
     });
 
     describe('Parallel tests', function () {
+        it('should work with empty empty functions list', function () {
+            var callback = sinon.spy();
+            flow.parallel([], callback);
+            callback.should.have.been.called;
+        });
+
         it('should call callback with correct results', function () {
             var callback = sinon.spy();
             var functions = [
@@ -57,7 +84,7 @@ describe('Flow tests', function () {
                 }
             ];
             flow.parallel(functions, callback);
-            assert.ok(callback.calledWith(null, [1, 2, 3]));
+            callback.should.have.been.calledWithExactly(null, [1, 2, 3]);
         });
 
         it('should call callback with error if needed', function () {
@@ -71,14 +98,14 @@ describe('Flow tests', function () {
                 }
             ];
             flow.parallel(functions, callback);
-            assert.ok(callback.calledOnce);
-            assert.equal(callback.firstCall.args[0], 'error');
+            callback.should.have.been.called.Once;
+            callback.should.have.been.calledWith('error');
         });
 
         it('should work with empty functions list', function () {
             var callback = sinon.spy();
             flow.parallel([], callback);
-            assert.ok(callback.calledWith(null, []));
+            callback.should.have.been.calledWithExactly(null, []);
         });
     });
 
@@ -89,7 +116,7 @@ describe('Flow tests', function () {
                 localCallback(null, number + 1);
             };
             flow.map([1, 2], func, callback);
-            assert.ok(callback.calledWith(null, [2, 3]));
+            callback.should.have.been.calledWithExactly(null, [2, 3]);
         });
 
         it('should call callback with error if needed', function () {
@@ -102,13 +129,13 @@ describe('Flow tests', function () {
                 }
             };
             flow.map([1, 2], func, callback);
-            assert.equal(callback.firstCall.args[0], 'error');
+            callback.should.have.been.calledWith('error');
         });
 
         it('should work with empty values list', function () {
             var callback = sinon.spy();
             flow.map([], function () {}, callback);
-            assert.ok(callback.calledWith(null, []));
+            callback.should.have.been.calledWithExactly(null, []);
         });
     });
 });
